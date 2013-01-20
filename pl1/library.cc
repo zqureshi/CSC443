9b8b949e961085c8d73d6ee5d37e94c8feef23bd
Zeeshan Qureshi <zee@zqureshi.in>
2013-01-20 07:28:36 -0500

    Add c++ skeleton for pl1.

---
+++ pl1/library.cc
@@ -0,0 +1,8 @@
+#include <cstdlib>
+
+/**
+ * Populate random array (which is already allocated with enough memory
+ * to hold n bytes).
+ */
+void random_array(char *array, long bytes) {
+}
---
+++ pl1/library.h
@@ -0,0 +1,5 @@
+/**
+ * Populate random array (which is already allocated with enough memory
+ * to hold n bytes).
+ */
+void random_array(char *array, long bytes);
