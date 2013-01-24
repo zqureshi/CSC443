f2a4b396e0fde143c2d5547198fdb125175e845f
Abhinav Gupta <abhinav@Abhinavs-MacBook-Pro.local>
2013-01-23 20:57:36 -0500

    Separated data collection and plotting scripts.

---
+++ 01-memory-hierarchy/collect.py
@@ -0,0 +1,83 @@
+import os
+import csv
+import shlex
+import argparse
+import subprocess
+from os import path
+
+FIRST_EXP = 10
+LAST_EXP  = 25
+FILE_SIZE = 104857600
+NOISE     = 'noise'
+
+GET_HISTOGRAM      = path.abspath('get_histogram')
+CREATE_RANDOM_FILE = path.abspath('create_random_file')
+
+def runcmd(args):
+    """
+    Run the command specified by the given argument. `args` can be a string or
+    a list specifying the command to execute and its arguments.
+    """
+    if type(args) is str:
+        args = shlex.split(args)
+    args = map(str, args)
+    with open(os.devnull, 'w') as fnull:
+        proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=fnull)
+        return proc.stdout.read()
+
+def get_histogram(blk):
+    """
+    Execute `get_histogram` with the given block size and return the amount of
+    time it took.
+    """
+    args = [GET_HISTOGRAM, NOISE, blk]
+    return int(runcmd(args).split()[1])
+
+def create_random_file(blk):
+    """
+    Use `create_random_file` to create a file using the given block size and
+    return the amount of time it took to complete the operation.
+    """
+    args = [CREATE_RANDOM_FILE, NOISE, FILE_SIZE, blk]
+    return int(runcmd(args).split()[1])
+
+def main():
+    parser = argparse.ArgumentParser(
+            description=('Runs create_random_file and get_histogram on the '
+                'with varying block sizes and outputs a CSV in the format: '
+                'BLOCK SIZE, CREATE TIME, GET TIME'))
+
+    parser.add_argument('--first-exponent', '-f', type=int, default=FIRST_EXP,
+            help='Exponent of 2 at which the block sizes will begin.',
+            dest='fexp', metavar='INT')
+
+    parser.add_argument('--last-exponent', '-l', type=int, default=LAST_EXP,
+            help='Exponent of 2 at which the block sizes will end.',
+            dest='lexp', metavar='INT')
+
+    parser.add_argument('--repeat', '-r', type=int, default=1,
+            help=('Number of times a command is executed with a specific '
+                'buffer size. Run times are averaged and rounded off to the '
+                'nearest integer. Default: 1'))
+
+    parser.add_argument('file', type=argparse.FileType('wb'), metavar='FILE',
+            help='File to which the collected data will be written.')
+
+    args = parser.parse_args()
+
+    repeat = args.repeat
+    writer = csv.writer(args.file)
+    for exp in xrange(args.fexp, args.lexp + 1):
+        block_size = 2 ** exp
+
+        write_times = [create_random_file(block_size) for i in xrange(repeat)]
+        read_times  = [get_histogram(block_size)      for i in xrange(repeat)]
+
+        write_time = sum(write_times) / repeat
+        read_time  = sum(read_times)  / repeat
+
+        writer.writerow([block_size, write_time, read_time])
+        os.remove(NOISE)
+
+if __name__ == '__main__':
+    main()
---
+++ 01-memory-hierarchy/plot.py
@@ -0,0 +1,72 @@
+import csv
+import argparse
+import operator
+from numpy import log2
+import matplotlib.pyplot as plt
+
+SND = operator.itemgetter(1)
+
+def main():
+
+    parser = argparse.ArgumentParser(
+            description='Plot the data collected by collect.py')
+
+    parser.add_argument('file', type=argparse.FileType('rb'), metavar='FILE',
+            help="File containing the data collected by collect.py.")
+    parser.add_argument('--output', '-o', metavar='FILE', default=None,
+            help=('Output file containing the plot. If absent, the plot will '
+                'be shown in a window.'))
+
+    args = parser.parse_args()
+
+    rows = (map(int, row) for row in csv.reader(args.file))
+    block_sizes, write_times, read_times = zip(*rows)
+
+    # Block sizes with the minimum write and read times
+    min_write = min(zip(block_sizes, write_times), key=SND)[0]
+    min_read  = min(zip(block_sizes,  read_times), key=SND)[0]
+
+    fig = plt.figure()
+    fig.suptitle('Hard Drive; File Size: 100MB')
+
+    # Plot write times
+    w_plot = fig.add_subplot(111)
+    (w_line,) = w_plot.semilogx(block_sizes, write_times, 'b.-', basex=2)
+    w_plot.set_xlabel('Block size')
+    w_plot.set_ylabel('Write time (in milliseconds)', color='b')
+
+    # Mark off block size with minimum write times
+    w_plot.axvline(min_write, color='b', linestyle=':', ymin=0.05)
+    w_plot.annotate('$2^{%d}$' % log2(min_write), xytext=(0, 3), fontsize=15,
+            xy=(min_write, w_plot.get_ylim()[0]), textcoords='offset points',
+            verticalalignment='bottom', horizontalalignment='center')
+
+    # Color the write time ticks blue
+    for t in w_plot.get_yticklabels():
+        t.set_color('b')
+
+    # Plot read times
+    r_plot = w_plot.twinx()
+    (r_line,) = r_plot.semilogx(block_sizes, read_times, 'g.-', basex=2)
+    r_plot.set_ylabel('Read time (in milliseconds)', color='g')
+    
+    # Mark off block size with minimum read times
+    r_plot.axvline(min_read, color='g', linestyle=':', ymax=0.95)
+    r_plot.annotate('$2^{%d}$' % log2(min_read), xytext=(0, -3), fontsize=15,
+            xy=(min_read, r_plot.get_ylim()[1]), textcoords='offset points',
+            verticalalignment='top', horizontalalignment='center')
+
+    # Color the read time ticks greed
+    for t in r_plot.get_yticklabels():
+        t.set_color('g')
+
+    # Add a legend
+    fig.legend((w_line, r_line), ('Write', 'Read'))
+
+    if args.output is not None:
+        fig.savefig(args.output)
+    else:
+        plt.show()
+
+if __name__ == '__main__':
+    main()
