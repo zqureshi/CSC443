497f76ee4a1f551dbb11e8dc32ff3f32da7384bb
Zeeshan Qureshi <zee@zqureshi.in>
2013-02-10 02:22:41 -0500

    Refactor page initialization into new method.

--- pl2/heapmanager.cc
+++ pl2/heapmanager.cc
@@ -15,6 +15,12 @@ inline int _get_new_id() {
     return id++;
 }
 
+Page *_init_page(Heapfile *heapfile) {
+    Page *page = new Page;
+    init_fixed_len_page(page, heapfile->page_size, heapSchema.numAttrs * heapSchema.attrLen);
+    return page;
+}
+
 /**
  * Initalize a heapfile to use the file and page size given.
  */
@@ -24,8 +30,7 @@ void init_heapfile(Heapfile *heapfile, int page_size, FILE *file, bool newHeap) 
     heapfile->page_size = page_size;
 
     /* Initialize primary directory page */
-    Page directory;
-    init_fixed_len_page(&directory, page_size, heapSchema.numAttrs * heapSchema.attrLen);
+    Page *directory = _init_page(heapfile);
 
     /* If creating new heap then write primary directory page */
     if(newHeap) {
@@ -37,19 +42,19 @@ void init_heapfile(Heapfile *heapfile, int page_size, FILE *file, bool newHeap) 
         dirHdr->sig2 = DIRHDR_SIG2;
 
         /* Populate directory header and make sure it's at the first slot */
-        assert(0 == add_fixed_len_page(&directory, &hdrRecord, heapSchema));
+        assert(0 == add_fixed_len_page(directory, &hdrRecord, heapSchema));
 
         /* Write page to disk */
         fseek(file, 0, SEEK_SET);
-        fwrite(directory.data, page_size, 1, file);
+        fwrite(directory->data, page_size, 1, file);
         fflush(file);
     } else {
         /* Otherwise validate that heap file has proper directory record */
         fseek(file, 0, SEEK_SET);
-        fread(directory.data, page_size, 1, file);
+        fread(directory->data, page_size, 1, file);
 
         Record hdrRecord(heapSchema);
-        assert(true == read_fixed_len_page(&directory, 0, &hdrRecord, heapSchema));
+        assert(true == read_fixed_len_page(directory, 0, &hdrRecord, heapSchema));
 
         DirHdr *dirHdr = (DirHdr *) (hdrRecord.at(0));
         assert(dirHdr->sig1 == DIRHDR_SIG1);
@@ -57,5 +62,5 @@ void init_heapfile(Heapfile *heapfile, int page_size, FILE *file, bool newHeap) 
     }
 
     /* Release memory for pages */
-    delete [] (char *) (directory.data);
+    delete [] (char *) (directory->data);
 }
